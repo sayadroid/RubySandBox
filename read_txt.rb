3bef1662583650761d59832b7b63a063dd029d48
SayaTsutsumi <saya.tsutsumi@gmail.com>
2014-04-16 16:45:06 +0900

    メソッド化

--- read_txt.rb
+++ read_txt.rb
@@ -1,10 +1,9 @@
-pattern = Regexp.new(ARGV[0])
-filename = ARGV[1]
-
-file = File.open(filename)
-file.each_line do |line|
-    if pattern =~ line
-        print line
+def simple_grep(pattern, filename)
+    file = File.open(filename)
+    file.each_line do |line|
+        if pattern =~ line
+            print line
+        end
     end
+    file.close
 end
-file.close
