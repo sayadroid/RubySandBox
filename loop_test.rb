e198914dcc430e1b522475a5b29a62a7061d2b32
SayaTsutsumi <saya.tsutsumi@gmail.com>
2014-04-17 11:39:40 +0900

    yieldを使ってみたかった

--- loop_test.rb
+++ loop_test.rb
@@ -16,3 +16,16 @@ names.each do |lang|
     end
     p [i, lang]
 end
+
+def myloop
+    while true
+        yield
+    end
+end
+
+num = 1
+myloop do
+    puts "num is #{num}"
+    break if num > 100
+    num *= 2
+end
